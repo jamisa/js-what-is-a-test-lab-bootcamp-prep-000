a5baf49162aedb350f934fce672edb25f79b06c7
Jamisa Fletcher <jamisafletcher@gmail.com>
2018-02-08 19:51:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var name = "Susan"
 var height = 35
-var message = ` is ${height} inches tall`
+var message = ` ${name} is ${height} inches tall`
 console.log(message)
 module.exports = {name, height, message}
 
